ebb0feb630a9d0e7687c0f4dd7ff63f61522b371
Utkarsh Singh <6995377+vib795@users.noreply.github.com>
2024-09-04 10:33:25 -0500

    Update app.py

--- browse-files-for-words/app.py
+++ browse-files-for-words/app.py
@@ -12,7 +12,8 @@ def is_utf8_decodable(file_path):
     except UnicodeDecodeError:
         return False
 
-def search_files(directory, search_word, output_file):
+def search_files(directory, search_word):
+    output_file = f"output-{search_word}.txt"
     with open(output_file, 'w', encoding='utf-8') as out_file:
         for root, dirs, files in os.walk(directory):
             # Remove .git directories from the dirs list to prevent descending into them
@@ -36,13 +37,13 @@ def search_files(directory, search_word, output_file):
                             out_file.write(f"{relative_path}-{matches}\n")
                 except Exception as e:
                     print(f"Error reading file {file_path}: {str(e)}")
+    return output_file
 
 # Get user input
-directory = input("Enter the directory path to search: ")
+directory = input("Enter the directory path to search (e.g., C:\\Users\\user\\github\\1234556\\terraform): ")
 search_word = input("Enter the word to search for: ")
-output_file = input("Enter the name of the output file: ")
 
 # Run the search
-search_files(directory, search_word, output_file)
+output_file = search_files(directory, search_word)
 
 print(f"Search complete. Results written to {output_file}")
